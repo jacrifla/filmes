176e20559491dd2479537a198fd66a32e850bc15
jacrifla <jaquelini_cris@hotmail.com>
2024-11-18 15:27:13 -0300

    atualizacao

--- other/config.js
+++ other/config.js
@@ -1,2 +1,2 @@
-// export const BASE_API_URL = 'http://localhost:4000';
-export const BASE_API_URL = 'https://server-filmes.onrender.com';
+export const BASE_API_URL = 'http://localhost:4000';
+// export const BASE_API_URL = 'https://server-filmes.onrender.com';
